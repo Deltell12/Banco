60d1c54ae9f1d66a75540525e37c052961f1ead5
Deltell GIT <adrideltelllopez12@gmail.com>
2025-03-04 13:49:56 +0100

    Banco

--- src/main/java/org/und4/DAWBank/CuentaBancaria.java
+++ src/main/java/org/und4/DAWBank/CuentaBancaria.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class CuentaBancaria {
     private String IBAN = creadorIBAN();
     String titular;
-    double saldo = 0;
-    ArrayList<String> movimientos;
+    static double saldo = 0;
+    static ArrayList<String> movimientos = new ArrayList<>();
 
-    public CuentaBancaria(String IBAN, String titular) {
+    public CuentaBancaria(String IBAN, String dni) {
         this.IBAN = IBAN;
-        this.titular = titular;
+        this.titular = dni;
     }
 
     public String getTitular() {
@@ -18,6 +18,7 @@ public class CuentaBancaria {
     }
 
     public void setTitular(String titular) {
+
         this.titular = titular;
     }
 
@@ -47,6 +48,44 @@ public class CuentaBancaria {
             IBAN += String.valueOf((int) (Math.random()*10));
         }
         return "ES"+IBAN;
+    }
+
+    public static void ingreso(double cantidad, String concepto){
+        String movimiento;
+
+        if (cantidad > 3000){
+            System.out.println("¡¡AVISO!! Al tratarse de un movimiento superior a los 3000€ procederemos a noticar a hacienda.");
+        }
+
+        else if (cantidad < 0){
+            throw new IllegalArgumentException("La cantidad no puede ser negativa.");
+        }
+
+        saldo = saldo + cantidad;
+        movimiento = (String.format("%-40s | %-10s | %-10s", (movimientos.size()+1)+"# "+concepto, "+"+cantidad + "€", "Saldo: "+saldo + "€"));
+
+        System.out.println("--Ingreso realizado con éxito--");
+
+        movimientos.add(movimiento);
+    }
+
+    public static void retirada(double cantidad, String concepto){
+        String movimiento;
+        saldo = saldo - cantidad;
+
+        if(saldo < -50){
+            throw new IllegalArgumentException("No se puede realizar una retirada si tu saldo es menor a -50€");
+        }
+
+        else if (cantidad < 0){
+            throw new IllegalArgumentException("LA cantidad no puede ser negativa.");
+        }
+
+        else{
+            movimiento = (String.format("%-40s | %-10s | %-10s", movimientos.size()+1+"# "+concepto, "-"+cantidad + "€", "Saldo: "+saldo + "€"));
+            System.out.println("--Retirada realizada con éxito--");
+        }
+        movimientos.add(movimiento);
     }
 
 
--- src/main/java/org/und4/DAWBank/DAWBank.java
+++ src/main/java/org/und4/DAWBank/DAWBank.java
@@ -1,9 +1,11 @@
 package org.und4.DAWBank;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import static org.und4.DAWBank.Cliente.*;
 
 public class DAWBank {
     String dni;
@@ -25,7 +27,7 @@ public class DAWBank {
 
         Scanner sc = new Scanner(System.in);
 
-        int opcion = 0;
+        String opcion = "";
         int intentos = 3;
         String usuario = "Adri";
         String contrasenia = "123";
@@ -42,42 +44,70 @@ public class DAWBank {
 
                     System.out.println("--MENU DE INICIO--");
                     System.out.println("1->\tNuevo cliente");
-                    System.out.println("2->\tCrear cuenta");
-                    System.out.println("3->\tConsultar cuenta");
-                    System.out.println("4->\tConsultar movimientos");
-                    System.out.println("5->\tOperaciones");
-                    System.out.println("6->\tSalir");
+                    System.out.println("2->\tDar cliente de baja");
+                    System.out.println("3->\tCrear cuenta");
+                    System.out.println("4->\tEliminar cuenta");
+                    System.out.println("5->\tConsultar cuenta");
+                    System.out.println("6->\tConsultar movimientos");
+                    System.out.println("7->\tOperaciones");
+                    System.out.println("8->\tSalir");
                     System.out.println();
 
                     System.out.print("Introduce una opción: ");
-                    opcion = sc.nextInt();
+                    opcion = sc.nextLine();
 
                     switch (opcion) {
 
-                        case 1:
+                        case "1":
                             nuevoCliente();
+                            System.out.println();
                             break;
 
-                        case 2:
+                        case "2":
+                            eliminarCliente();
+                            System.out.println();
+                            break;
+
+                        case "3":
                             nuevaCuenta();
+                            System.out.println();
                             break;
 
-                        case 3:
+                        case "4":
+                            eliminarCuenta();
+                            System.out.println();
+                            break;
+
+                        case "5":
                             consultarCuenta();
+                            System.out.println();
                             break;
 
-                        case 4:
+                        case "6":
                             consultarMovimientos();
+                            System.out.println();
                             break;
 
-                        case 6:
+                        case "7":
+                            operaciones();
+                            System.out.println();
+                            break;
+
+                        case "8":
+                            System.out.println("Saliendo...");
+                            break;
+
+                        default:
+                            System.out.println("La opción no es valida");
+                            System.out.println();
                             break;
 
                     }
 
 
                 }
-                while (opcion != 6);
+                while (!opcion.equals("8"));
+                break;
             } else {
                 System.out.println("Error, uno de los datos no es correcto.");
                 intentos--;
@@ -98,26 +128,59 @@ public class DAWBank {
         try {
             System.out.print("Introduce tu DNI: ");
             String dni = sc.next();
+            sc.nextLine();
             if (clientes.containsKey(dni)) {
                 throw new IllegalArgumentException("Error, ya existe un cliente con ese DNI.");
             }
             System.out.print("Introduce tu nombre completo: ");
-            String nombre = sc.next();
+            String nombre = sc.nextLine();
             System.out.print("Introduce tu correo electronico: ");
-            String correo = sc.next();
+            String correo = sc.nextLine();
+
             for (Map.Entry<String, Cliente> entry : clientes.entrySet()) {
                 if (entry.getValue().getCorreoElectronico().equals(correo)) {
                     throw new IllegalArgumentException("Error, ya existe un cliente con ese correo.");
                 }
             }
             Cliente cliente = new Cliente(dni, nombre, correo);
-            clientes.put(dni, new Cliente(cliente));
+            clientes.put(dni, (cliente));
         } catch (Exception e) {
             System.out.println(e.getMessage());
 
         }
     }
 
+    public static void eliminarCliente() {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+
+            System.out.print("Introduce el DNI asociado al cliente que desea dar de baja: ");
+            String dni = sc.next();
+            if (!Cliente.verificarDNI(dni)) {
+                throw new IllegalArgumentException("Error,el DNI debe de ser válido");
+            }
+            if (clientes.containsKey(dni)) {
+                System.out.print("¿Está seguro que desea dar de baja a este cliente? (True/False): ");
+                boolean preguntaEliminar = sc.nextBoolean();
+
+                if (preguntaEliminar) {
+                    System.out.println("Dando de baja al cliente seleccionado...");
+                    clientes.remove(dni);
+                    System.out.println("Cliente dado de baja correctamente.");
+                }
+                else {
+                    System.out.println("Operación cancelada");
+                }
+            }
+            else {
+                System.out.println("Error, no hay ningun cliente con ese DNI.");
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
     public static void nuevaCuenta() {
         Scanner sc = new Scanner(System.in);
 
@@ -125,9 +188,10 @@ public class DAWBank {
         String dni = sc.next();
 
         if (clientes.containsKey(dni)) {
-                String IBAN = CuentaBancaria.creadorIBAN();
-                System.out.println("Su IBAN es: " +IBAN);
-                cuentas.put(IBAN, new CuentaBancaria(IBAN, dni));
+            String IBAN = CuentaBancaria.creadorIBAN();
+            CuentaBancaria C1 = new CuentaBancaria(IBAN, dni);
+            System.out.println("Su IBAN es: " +IBAN);
+            cuentas.put(IBAN, C1);
 
         } else {
                 System.out.println("Error, no existe ningún cliente con ese DNI.");
@@ -136,23 +200,61 @@ public class DAWBank {
             }
         }
 
+    public static void eliminarCuenta() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce el IBAN asociado a la cuenta que desea eliminar: ");
+        String iban = sc.next();
+        try {
+
+            if (cuentas.containsKey(iban)) {
+                System.out.print("¿Está seguro que desea eliminar esta cuenta? (True/False): ");
+                boolean preguntaEliminar = sc.nextBoolean();
+
+                if (preguntaEliminar) {
+                    System.out.println("Eliminando cuenta seleccionada...");
+                    cuentas.remove(iban);
+                    System.out.println("Cuenta eliminada correctamente.");
+                } else {
+                    System.out.println("Operación cancelada");
+                }
+            }
+            else {
+                System.out.println("Error, no hay ninguna cuenta con ese IBAN.");
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void consultarCuenta() {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Introduce tu IBAN: ");
         String iban = sc.nextLine();
 
-        for (Map.Entry<String, CuentaBancaria> entradaCuenta : cuentas.entrySet()) {
+        try {
+
+
+            for (Map.Entry<String, CuentaBancaria> entradaCuenta : cuentas.entrySet()) {
 
                 if (entradaCuenta.getValue().getIBAN().equals(iban)) {
-                    System.out.println("IBAN: "+entradaCuenta.getValue().getIBAN() + " | Nombre Titular: " + clientes.get(entradaCuenta.getValue().getTitular()).getNombreCompleto() + " | Saldo de la cuenta: " + entradaCuenta.getValue().getSaldo() + " | Fecha de la consulta: " + hoy);
+                    System.out.println("IBAN: " + entradaCuenta.getValue().getIBAN() + " | Nombre Titular: " + clientes.get(entradaCuenta.getValue().getTitular()).getNombreCompleto() + " | Saldo de la cuenta: " + entradaCuenta.getValue().getSaldo() + "€ | Fecha de la consulta: " + hoy);
+                    if (clientes.get(entradaCuenta.getValue().getTitular()).getNombreCompleto() == null) {
+                        System.out.println("No existe ninguna cuenta asociada a este IBAN");
+                        System.out.println("--Logueate como Cliente--");
+                        nuevoCliente();
+                    }
                     return;
                 }
             }
-        System.out.println("Error, no existe ninguna cuenta con ese IBAN.");
-        System.out.println("--Create una Cuenta--");
-        nuevaCuenta();
+            System.out.println("Error, no existe ninguna cuenta con ese IBAN.");
+            System.out.println("--Create una Cuenta--");
+            nuevaCuenta();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+    }
 
     public static void consultarMovimientos() {
         Scanner sc = new Scanner(System.in);
@@ -163,20 +265,78 @@ public class DAWBank {
         try {
             for (Map.Entry<String, CuentaBancaria> entradaCuenta : cuentas.entrySet()) {
                 if (entradaCuenta.getValue().getIBAN().equals(iban)) {
-                    System.out.println(entradaCuenta.getValue().movimientos);
+                    Collections.reverse(entradaCuenta.getValue().getMovimientos());
+                    for (String parte : CuentaBancaria.movimientos) {
+                        System.out.println(parte);
+                    }
                     if (entradaCuenta.getValue().getMovimientos().isEmpty()) {
                         throw new IllegalArgumentException("Error, no hay movimientos.");
                     }
                 }
+                else{
+                    System.out.println("Error, no existe ninguna cuenta con ese IBAN.");
+                    System.out.println("--Create una Cuenta--");
+                    nuevaCuenta();
+                }
             }
-            System.out.println("Error, no existe ninguna cuenta con ese IBAN.");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void operaciones() {
+        Scanner sc = new Scanner(System.in);
+        int contador = 0;
+
+        System.out.print("Introduce tu IBAN: ");
+        String iban = sc.nextLine();
+        if (!cuentas.containsKey(iban)) {
+            System.out.println("No existe ninguna cuenta con este IBAN.");
             System.out.println("--Create una Cuenta--");
             nuevaCuenta();
+        }
+
+
+        try {
+
+        for (Map.Entry<String, CuentaBancaria> entradaCuenta : cuentas.entrySet()) {
+            if (entradaCuenta.getValue().getIBAN().equals(iban)) {
+                System.out.println("1. INGRESO");
+                System.out.println("2. RETIRADA");
+                System.out.print("Introduce una opción: ");
+                String opcion = sc.nextLine();
+                switch (opcion) {
+                    case "1":
+                        System.out.print("Introduce el concepto del ingreso: ");
+                        String conceptoIngresos = sc.nextLine();
+                        System.out.print("Introduce la cantidad del ingreso: ");
+                        double cantidadIngreso = sc.nextDouble();
+
+                        CuentaBancaria.ingreso(cantidadIngreso, conceptoIngresos);
+
+                        break;
+
+                    case "2":
+                        System.out.print("Introduce el concepto del ingreso: ");
+                        String conceptoRetirada = sc.nextLine();
+                        System.out.print("Introduce la cantidad del ingreso: ");
+                        double cantidadRetirada = sc.nextDouble();
+
+                        CuentaBancaria.retirada(cantidadRetirada, conceptoRetirada);
+
+                        break;
+
+                    default:
+                        System.out.println("La opción seleccionada no es válida.");
+                        break;
+                }
+            }
+
+            }
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 }
-
-
